4b1d0649913167f78b559d45701faa4e60fcb6b0
shivalasvegas <shiva.king@academytrainee.com>
2020-07-29 14:52:41 +0100

    SK - MyController: Added delete and post methods

--- QAConsulting/src/main/java/com/qa/qaconsulting/controller/MyController.java
+++ QAConsulting/src/main/java/com/qa/qaconsulting/controller/MyController.java
@@ -3,8 +3,11 @@ package com.qa.qaconsulting.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.qa.qaconsulting.entity.Student;
@@ -14,7 +17,7 @@ import com.qa.qaconsulting.repository.StudentRepository;
 @RestController
 public class MyController {
 	
-	@Autowired
+	@Autowired // injects stuRepo
 	StudentRepository stuRepo;
 	
 	// first 
@@ -50,8 +53,20 @@ public class MyController {
 		ref2.setStudentAddress(address);
 		stuRepo.save(ref2);
 		return "The record for " + ref2.getStudentName() + " is now saved!";
-		
-		
+			
 	}
-
+	
+	@DeleteMapping("deleterecord/{id}")
+	public String delete(@PathVariable(value="studentId") int id) {
+		Student ref3 = new Student();
+		ref3.setStudentId(id);
+		stuRepo.deleteById(ref3.getStudentId());
+		return "Deleted record with id: " + ref3.getStudentId();
+	
+	}
+	
+	@PostMapping("insertrecord/")
+	public String insertRecord(@RequestBody Student student1) {
+		return student1.getStudentId() + " " + student1.getStudentName() + " " +  student1.getStudentAddress();
+	}
 }
