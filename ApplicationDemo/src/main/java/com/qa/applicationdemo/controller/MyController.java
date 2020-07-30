99ec201df5c8aa3d51fed8029b63d591ef73b11c
shivalasvegas <shiva.king@academytrainee.com>
2020-07-30 10:55:02 +0100

    SK - SalesService.java: Added CRUD methods

--- ApplicationDemo/src/main/java/com/qa/applicationdemo/controller/MyController.java
+++ ApplicationDemo/src/main/java/com/qa/applicationdemo/controller/MyController.java
@@ -2,6 +2,8 @@ package com.qa.applicationdemo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.qa.applicationdemo.services.SalesService;
@@ -13,6 +15,24 @@ public class MyController {
 	public MyController(SalesService service) {
 		this.service = service;
 	}
+	
+	@PostMapping("/create")
+	public String insertRecord(@RequestBody SalesService service) {
+	service.save(service);
+
+	return "record made";
+}
+	
+	
+//	/	@GetMapping("/readall")
+//	public List<Student> showAllRecords() {
+//		List<Student> records = stuRepo.findAll();
+//		
+//		return records;
+//		
+//	}
+	
+	
 	
 	@GetMapping("/delete/{id}")
 	public String deleteRecord(@PathVariable("salesId") int id) {
--- ApplicationDemo/src/main/java/com/qa/applicationdemo/services/SalesService.java
+++ ApplicationDemo/src/main/java/com/qa/applicationdemo/services/SalesService.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.qa.applicationdemo.entity.Sale;
 import com.qa.applicationdemo.repository.SalesRepo;
@@ -15,22 +16,48 @@ public class SalesService {
 	@Autowired 
 	SalesRepo salesRepo;
 	
-	public void storeRecord() {
-		
-		Sale saleItem = new Sale();
-		saleItem.setProductName("Coke");
-		saleItem.setQuantity(12);
+//	public void storeRecord() {
+//		
+//		Sale saleItem = new Sale();
+//		saleItem.setProductName("Coke");
+//		saleItem.setQuantity(12);
+//		salesRepo.save(saleItem);
+//	}
+	
+//Create
+	public String createRecord(@RequestBody Sale saleItem) {
 		salesRepo.save(saleItem);
+		
+		return "Record made";
 	}
 	
-	public String deleteRecord(int a) {
+	//Read
+
+	public Optional<Sale> readSpecific(int id) {
+		
+		//Optional<Student> opt = stuRepo.findById(id);
+		if (salesRepo.existsById(id)) {
+			Optional<Sale> ref = salesRepo.findById(id);
+			return ref;
+			
+		}
+		return null;
+	}
+	
+	//Update
+	
+	
+	//delete
+	public String deleteRecord(int id) {
 		String message;	
-		Optional<Sale> record = salesRepo.findById(a);
+		Optional<Sale> record = salesRepo.findById(id);
 		
 		if (record.isPresent()) {
-			salesRepo.deleteById(a);
+			salesRepo.deleteById(id);
 			message =  "Record deleted";
 		}else message = "Id does not exsit";
 		return message;
 }
+	
+	
 }
